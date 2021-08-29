54357e9b2aedbb509117c9aebbcdb2d7a960f731
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-08-29 21:44:18 +0200

    check if deploy on success pytest -> hello world

--- farm/app.py
+++ farm/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route("/")
 def index():
-    return "check if test comes through"
+    return "hello world"
 
 
 @app.route("/cow")
